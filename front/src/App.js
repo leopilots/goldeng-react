d4d0e32c0c8770e7c228e099292808e4449244a9
leopilots <leonardoyalejandrosr@gmail.com>
2024-09-30 14:58:08 -0400

    add header. fixed navegation

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-
-import Navegation from './components/navegation';
+import Navegation from './components/navegation.js';
+import Header from './components/Header.js';
 
 function App() {
     return (
         <div className="App">
             <Navegation />
+            <Header />
         </div>
     );
 }
--- front/src/components/navegation.js
+++ front/src/components/navegation.js
@@ -1,46 +1,48 @@
-import React from 'react'
-
-import '../css/navegation.css';
-
-document.querySelector(".menu-btn").addEventListener("click", () => {
-    document.querySelector(".nav-main").classList.toggle("show");
-});
+import React, { useEffect } from 'react';
+import '../css/Navegation.css';
 
 export default function Navegation() {
+    useEffect(() => {
+        const menuBtn = document.querySelector(".menu-btn");
+        const navMain = document.querySelector(".nav-main");
+
+        if (menuBtn && navMain) {
+            const toggleMenu = () => {
+                navMain.classList.toggle("show");
+            };
+
+            menuBtn.addEventListener("click", toggleMenu);
+
+            return () => {
+                menuBtn.removeEventListener("click", toggleMenu);
+            };
+        }
+    }, []);
+
     return (
-    <div>
-        <div className='promotion'>
-            <p>¡Disfruta de envío gratis en tu primera compra! <a href="#">Compra ahora</a>.</p>
-        </div>
-        <div className='nameBrand'>
-            <a href='#'><h1>Golden G</h1></a>
-        </div>
-        <nav>
-            <div className='menu-btn'>
-                <i class="fa-solid fa-bars"></i>
+        <div>
+            <div className='promotion'>
+                <p>¡Disfruta de envío gratis en tu primera compra! <a href="#">Compra ahora</a>.</p>
             </div>
-            <div className='nav-main'>
-                <ul className='nav-menu'>
-                    <li>
-                        <a href='#'>Hombre</a>
-                    </li>
-                    <li>
-                        <a href='#'>Mujer</a>
-                    </li>
-                    <li>
-                        <a href='#'>Más Vendidos</a>
-                    </li>
-                </ul>
-                <ul className='nav-menu'>
-                    <li>
-                        <a href='#'>Mi carrito</a>
-                    </li>
-                    <li>
-                        <a href='#'>Mi Cuenta</a>
-                    </li>
-                </ul>
+            <div className='nameBrand'>
+                <a href='#'><h1>Golden G</h1></a>
             </div>
-        </nav>
-    </div>
-  )
+            <nav>
+                <div className='menu-btn'>
+                    <i className="fa-solid fa-bars"></i>
+                </div>
+                <div className='nav-main'>
+                    <ul className='nav-menu'>
+                        <li><a href='#'>Hombre</a></li>
+                        <li><a href='#'>Mujer</a></li>
+                        <li><a href='#'>Más Vendidos</a></li>
+                    </ul>
+                    <ul className='nav-menu'>
+                        <li><a href='#'>Mi Cuenta</a></li>
+                        <li><a href='#'>Mi Cesta</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    );
 }
